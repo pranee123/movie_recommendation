4e59716a026a1a6242a2a81cb4dd10e971804f91
Pranee123 <praneethgoud2510@gmail.com>
2024-10-04 19:19:10 +0530

    Updated movies File

--- movies.py
+++ movies.py
@@ -11,10 +11,21 @@ load_dotenv() # Activate the Local Environment
 genai.configure(api_key=os.getenv("Google-Api-Key"))
 
 # Designing the Webpage...
-st.title("👀Movie Recommender Systems")
+st.title("👀🎬Movie Recommender Systems👀")
 user_input = st.text_input("Enter the Movie Title,Genere, songs or Keyword👇")
-demo_template = '''Based on {user_input} give me Movie Recommendations and sort \
-the recommendations by geners, movie songs {user_input}'''
+demo_template = '''
+Based on your interest in "{user_input}", here are some personalized movie recommendations for you:
+
+1. We have categorized the recommendations based on:
+   - Genre: The movies are sorted first by genre to help you explore your preferred type of films.
+   - Songs: Along with each movie, you'll also get recommendations on notable songs featured in these films.
+
+2. Below are the sorted recommendations:
+   - Genre: {user_input}}
+   - Movie Songs: {user_input}
+
+Enjoy your movie time!
+'''
 
 template = PromptTemplate(input_variables=['user_input'],template=demo_template)
 
