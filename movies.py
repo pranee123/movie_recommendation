351d1d7ae41486eaeed816ad0e5c5863cc997fdd
Pranee123 <praneethgoud2510@gmail.com>
2024-10-04 20:21:40 +0530

    Updated movies.py with enhanced movie recommendations

--- movies.py
+++ movies.py
@@ -11,11 +11,11 @@ load_dotenv() # Activate the Local Environment
 genai.configure(api_key=os.getenv("Google-Api-Key"))
 
 # Designing the Webpage...
-st.title("🎬🎬Movie Recommender Systems")
-user_input = st.text_input("Enter the Movie Title,Genre, songs or Keyword👇")
+st.title("🎬Movie Recommender Systems")
+user_input = st.text_input("Enter the Movie Title,Genre, Songs or Keyword👇")
 demo_template = '''Based on your interest in "{user_input}", here are some personalized \
-movie recommendations for you:\
-Give me the popular songs from them and tell the story in short and recommend similar movies\
+movie recommendations for you: Give me the popular songs from them and tell the story in short \
+and recommend similar movies\
 Enjoy your movie time! '''
 
 template = PromptTemplate(input_variables=['user_input'],template=demo_template)
